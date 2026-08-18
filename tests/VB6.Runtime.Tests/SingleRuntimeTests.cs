0859140a058020a13b8f2df128e8ceb236cf1306
Artur Löwen <125445926+BlackFoX1991@users.noreply.github.com>
2026-08-18 14:28:26 +0200

    Test VB6 floating division errors

--- tests/VB6.Runtime.Tests/SingleRuntimeTests.cs
+++ tests/VB6.Runtime.Tests/SingleRuntimeTests.cs
@@ -19,9 +19,12 @@ public sealed class SingleRuntimeTests
     }
 
     [TestMethod]
-    public void SingleArithmetic_ReportsOverflowAndDivisionByZero()
+    public void FloatingDivision_UsesVb6RuntimeErrors()
     {
         Assert.ThrowsException<OverflowException>(() => VBOperators.MultiplySingle(float.MaxValue, 2f));
         Assert.ThrowsException<DivideByZeroException>(() => VBOperators.DivideSingle(1f, 0f));
+        Assert.ThrowsException<OverflowException>(() => VBOperators.DivideSingle(0f, 0f));
+        Assert.ThrowsException<DivideByZeroException>(() => VBOperators.DivideDouble(1d, 0d));
+        Assert.ThrowsException<OverflowException>(() => VBOperators.DivideDouble(0d, 0d));
     }
 }
