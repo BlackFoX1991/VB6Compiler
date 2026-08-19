fa3c28a4d42ef15d041adc5a30235d2eafc39942
Artur Löwen <125445926+BlackFoX1991@users.noreply.github.com>
2026-08-19 12:53:06 +0200

    Test VB array runtime storage

---
+++ tests/VB6.Runtime.Tests/VBArrayTests.cs
@@ -0,0 +1,59 @@
+using VB6.Runtime;
+
+namespace VB6.Runtime.Tests;
+
+[TestClass]
+public sealed class VBArrayTests
+{
+    [TestMethod]
+    public void Array_PreservesNonZeroLowerBounds()
+    {
+        var array = new VBArray<int>(new VBArrayBound(1, 3));
+
+        Assert.AreEqual(1, array.Rank);
+        Assert.AreEqual(3, array.Length);
+        Assert.AreEqual(1, array.LBound());
+        Assert.AreEqual(3, array.UBound());
+
+        array[1] = 10;
+        array[3] = 30;
+        Assert.AreEqual(10, array[1]);
+        Assert.AreEqual(30, array[3]);
+    }
+
+    [TestMethod]
+    public void Array_PreservesMultipleDimensions()
+    {
+        var array = new VBArray<string>(
+            new VBArrayBound(0, 1),
+            new VBArrayBound(5, 7));
+
+        Assert.AreEqual(2, array.Rank);
+        Assert.AreEqual(6, array.Length);
+        Assert.AreEqual(0, array.LBound(1));
+        Assert.AreEqual(1, array.UBound(1));
+        Assert.AreEqual(5, array.LBound(2));
+        Assert.AreEqual(7, array.UBound(2));
+
+        array[1, 6] = "ok";
+        Assert.AreEqual("ok", array[1, 6]);
+    }
+
+    [TestMethod]
+    public void Array_RejectsOutOfRangeSubscriptsAndDimensions()
+    {
+        var array = new VBArray<int>(new VBArrayBound(-2, 2));
+
+        Assert.ThrowsException<IndexOutOfRangeException>(() => _ = array[3]);
+        Assert.ThrowsException<IndexOutOfRangeException>(() => _ = array[0, 0]);
+        Assert.ThrowsException<IndexOutOfRangeException>(() => array.LBound(2));
+    }
+
+    [TestMethod]
+    public void Array_RejectsInvalidBounds()
+    {
+        Assert.ThrowsException<ArgumentOutOfRangeException>(() =>
+            new VBArray<int>(new VBArrayBound(5, 4)));
+        Assert.ThrowsException<ArgumentException>(() => new VBArray<int>());
+    }
+}
