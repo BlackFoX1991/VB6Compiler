4683206616dab2fce222dd294fe8d85c96e234cf
Artur Löwen <125445926+BlackFoX1991@users.noreply.github.com>
2026-08-19 14:55:31 +0200

    Add Erase statement syntax node

---
+++ src/VB6.Syntax/Nodes/EraseStatementSyntax.cs
@@ -0,0 +1,11 @@
+using System.Collections.Immutable;
+
+namespace VB6.Syntax.Nodes;
+
+/// <summary>
+/// A VB6 <c>Erase</c> statement over one or more comma-separated array variables.
+/// </summary>
+public sealed record EraseStatementSyntax(
+    SyntaxToken EraseKeyword,
+    ImmutableArray<SyntaxToken> Identifiers)
+    : StatementSyntax(SyntaxKind.EraseStatement);
