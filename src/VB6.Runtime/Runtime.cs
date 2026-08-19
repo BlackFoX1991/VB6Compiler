d725d012bc2f1995eeaf97d5c9178fba8322d28c
Artur Löwen <125445926+BlackFoX1991@users.noreply.github.com>
2026-08-19 22:48:40 +0200

    Split VB operators for Variant runtime support

--- src/VB6.Runtime/Runtime.cs
+++ src/VB6.Runtime/Runtime.cs
@@ -52,297 +52,196 @@ public static class VBConversions
             : Convert.ToInt64(value, CultureInfo.CurrentCulture);
     }
 
-    public static VBCurrency CCur(object? value)
-    {
-        if (value is VBCurrency currency)
-        {
-            return currency;
-        }
+    public static VBCurrency CCur(object? value) => VBCurrency.From(value);
 
+    public static float CSng(object? value) => Convert.ToSingle(value, CultureInfo.CurrentCulture);
+    public static double CDbl(object? value) => Convert.ToDouble(value, CultureInfo.CurrentCulture);
+    public static string CStr(object? value) => Convert.ToString(value, CultureInfo.CurrentCulture) ?? string.Empty;
+
+    public static bool CBool(object? value)
+    {
         if (value is bool boolean)
         {
-            return VBCurrency.FromScaled(boolean ? -VBCurrency.Scale : 0L);
+            return boolean;
         }
 
-        var decimalValue = Convert.ToDecimal(value, CultureInfo.CurrentCulture);
-        return VBCurrency.FromDecimal(decimalValue);
-    }
-
-    public static float CSng(object? value)
-    {
-        var result = value switch
+        if (value is VBCurrency currency)
         {
-            VBCurrency currency => currency.ToSingle(),
-            bool boolean => boolean ? -1f : 0f,
-            _ => Convert.ToSingle(value, CultureInfo.CurrentCulture)
-        };
-        return CheckSingle(result);
-    }
-
-    public static double CDbl(object? value) => value switch
-    {
-        VBCurrency currency => currency.ToDouble(),
-        bool boolean => boolean ? -1d : 0d,
-        _ => Convert.ToDouble(value, CultureInfo.CurrentCulture)
-    };
-
-    public static bool CBool(object? value) => value is VBCurrency currency
-        ? currency.ScaledValue != 0
-        : Convert.ToBoolean(value, CultureInfo.CurrentCulture);
-
-    public static string CStr(object? value) => value is VBCurrency currency
-        ? currency.ToString()
-        : Convert.ToString(value, CultureInfo.CurrentCulture) ?? string.Empty;
-
-    private static float CheckSingle(float value)
-    {
-        if (float.IsInfinity(value))
-        {
-            throw new OverflowException("Value is outside the range of VB6 Single.");
+            return currency.ScaledValue != 0;
         }
 
-        return value;
+        if (value is null)
+        {
+            return false;
+        }
+
+        if (value is string text)
+        {
+            if (bool.TryParse(text, out var parsedBoolean))
+            {
+                return parsedBoolean;
+            }
+
+            if (double.TryParse(text, NumberStyles.Float, CultureInfo.CurrentCulture, out var parsedNumber))
+            {
+                return parsedNumber != 0.0;
+            }
+        }
+
+        return Convert.ToDouble(value, CultureInfo.CurrentCulture) != 0.0;
     }
 }
 
-public static class VBOperators
+public static partial class VBOperators
 {
+    public static double Power(double left, double right) => Math.Pow(left, right);
+
+    public static bool Equal(object? left, object? right) => Compare(left, right) == 0;
+    public static bool NotEqual(object? left, object? right) => Compare(left, right) != 0;
+    public static bool Less(object? left, object? right) => Compare(left, right) < 0;
+    public static bool LessOrEqual(object? left, object? right) => Compare(left, right) <= 0;
+    public static bool Greater(object? left, object? right) => Compare(left, right) > 0;
+    public static bool GreaterOrEqual(object? left, object? right) => Compare(left, right) >= 0;
+
     public static byte AddByte(byte left, byte right) => checked((byte)(left + right));
+    public static short AddInteger(short left, short right) => checked((short)(left + right));
+    public static int AddLong(int left, int right) => checked(left + right);
+    public static long AddLongLong(long left, long right) => checked(left + right);
+    public static VBCurrency AddCurrency(VBCurrency left, VBCurrency right) => VBCurrency.Add(left, right);
+    public static float AddSingle(float left, float right) => CheckFinite(left + right);
+    public static double AddDouble(double left, double right) => left + right;
 
     public static byte SubtractByte(byte left, byte right) => checked((byte)(left - right));
+    public static short SubtractInteger(short left, short right) => checked((short)(left - right));
+    public static int SubtractLong(int left, int right) => checked(left - right);
+    public static long SubtractLongLong(long left, long right) => checked(left - right);
+    public static VBCurrency SubtractCurrency(VBCurrency left, VBCurrency right) => VBCurrency.Subtract(left, right);
+    public static float SubtractSingle(float left, float right) => CheckFinite(left - right);
+    public static double SubtractDouble(double left, double right) => left - right;
 
     public static byte MultiplyByte(byte left, byte right) => checked((byte)(left * right));
+    public static short MultiplyInteger(short left, short right) => checked((short)(left * right));
+    public static int MultiplyLong(int left, int right) => checked(left * right);
+    public static long MultiplyLongLong(long left, long right) => checked(left * right);
+    public static VBCurrency MultiplyCurrency(VBCurrency left, VBCurrency right) => VBCurrency.Multiply(left, right);
+    public static float MultiplySingle(float left, float right) => CheckFinite(left * right);
+    public static double MultiplyDouble(double left, double right) => left * right;
 
     public static byte IntegerDivideByte(byte left, byte right) => checked((byte)(left / right));
-
-    public static byte ModByte(byte left, byte right) => checked((byte)(left % right));
-
-    public static short AddInteger(short left, short right) => checked((short)(left + right));
-
-    public static short SubtractInteger(short left, short right) => checked((short)(left - right));
-
-    public static short MultiplyInteger(short left, short right) => checked((short)(left * right));
-
-    public static short NegateInteger(short value) => checked((short)-value);
-
     public static short IntegerDivide(short left, short right) => checked((short)(left / right));
-
-    public static short ModInteger(short left, short right) => checked((short)(left % right));
-
-    public static int AddLong(int left, int right) => checked(left + right);
-
-    public static int SubtractLong(int left, int right) => checked(left - right);
-
-    public static int MultiplyLong(int left, int right) => checked(left * right);
-
-    public static int NegateLong(int value) => checked(-value);
-
     public static int IntegerDivideLong(int left, int right) => checked(left / right);
-
-    public static int ModLong(int left, int right) => checked(left % right);
-
-    public static long AddLongLong(long left, long right) => checked(left + right);
-
-    public static long SubtractLongLong(long left, long right) => checked(left - right);
-
-    public static long MultiplyLongLong(long left, long right) => checked(left * right);
-
-    public static long NegateLongLong(long value) => checked(-value);
-
     public static long IntegerDivideLongLong(long left, long right) => checked(left / right);
 
-    public static long ModLongLong(long left, long right) => checked(left % right);
+    public static byte ModByte(byte left, byte right) => checked((byte)(left % right));
+    public static short ModInteger(short left, short right) => checked((short)(left % right));
+    public static int ModLong(int left, int right) => left % right;
+    public static long ModLongLong(long left, long right) => left % right;
 
-    public static VBCurrency AddCurrency(VBCurrency left, VBCurrency right) =>
-        VBCurrency.FromScaled(checked(left.ScaledValue + right.ScaledValue));
+    public static byte AndByte(byte left, byte right) => (byte)(left & right);
+    public static short AndInteger(short left, short right) => (short)(left & right);
+    public static int AndLong(int left, int right) => left & right;
+    public static long AndLongLong(long left, long right) => left & right;
+    public static bool AndBoolean(bool left, bool right) => left & right;
 
-    public static VBCurrency SubtractCurrency(VBCurrency left, VBCurrency right) =>
-        VBCurrency.FromScaled(checked(left.ScaledValue - right.ScaledValue));
+    public static byte OrByte(byte left, byte right) => (byte)(left | right);
+    public static short OrInteger(short left, short right) => (short)(left | right);
+    public static int OrLong(int left, int right) => left | right;
+    public static long OrLongLong(long left, long right) => left | right;
+    public static bool OrBoolean(bool left, bool right) => left | right;
 
-    public static VBCurrency MultiplyCurrency(VBCurrency left, VBCurrency right) =>
-        VBCurrency.Multiply(left, right);
+    public static byte XorByte(byte left, byte right) => (byte)(left ^ right);
+    public static short XorInteger(short left, short right) => (short)(left ^ right);
+    public static int XorLong(int left, int right) => left ^ right;
+    public static long XorLongLong(long left, long right) => left ^ right;
+    public static bool XorBoolean(bool left, bool right) => left ^ right;
 
-    public static VBCurrency NegateCurrency(VBCurrency value) =>
-        VBCurrency.FromScaled(checked(-value.ScaledValue));
+    public static byte EqvByte(byte left, byte right) => (byte)~(left ^ right);
+    public static short EqvInteger(short left, short right) => (short)~(left ^ right);
+    public static int EqvLong(int left, int right) => ~(left ^ right);
+    public static long EqvLongLong(long left, long right) => ~(left ^ right);
+    public static bool EqvBoolean(bool left, bool right) => left == right;
 
-    public static float AddSingle(float left, float right) => CheckSingle(left + right);
+    public static byte ImpByte(byte left, byte right) => (byte)(~left | right);
+    public static short ImpInteger(short left, short right) => (short)(~left | right);
+    public static int ImpLong(int left, int right) => ~left | right;
+    public static long ImpLongLong(long left, long right) => ~left | right;
+    public static bool ImpBoolean(bool left, bool right) => !left || right;
 
-    public static float SubtractSingle(float left, float right) => CheckSingle(left - right);
+    public static byte NotByte(byte operand) => (byte)~operand;
+    public static short NotInteger(short operand) => (short)~operand;
+    public static int NotLong(int operand) => ~operand;
+    public static long NotLongLong(long operand) => ~operand;
+    public static bool NotBoolean(bool operand) => !operand;
 
-    public static float MultiplySingle(float left, float right) => CheckSingle(left * right);
-
-    public static float NegateSingle(float value) => CheckSingle(-value);
+    public static byte NegateByte(byte operand) => checked((byte)-operand);
+    public static short NegateInteger(short operand) => checked((short)-operand);
+    public static int NegateLong(int operand) => checked(-operand);
+    public static long NegateLongLong(long operand) => checked(-operand);
+    public static VBCurrency NegateCurrency(VBCurrency operand) => VBCurrency.Negate(operand);
+    public static float NegateSingle(float operand) => -operand;
+    public static double NegateDouble(double operand) => -operand;
 
     public static float DivideSingle(float left, float right)
     {
         if (right == 0f)
         {
-            if (left == 0f)
-            {
-                throw new OverflowException("VB6 floating-point division 0 / 0 causes overflow.");
-            }
-
             throw new DivideByZeroException();
         }
 
-        return CheckSingle(left / right);
+        return CheckFinite(left / right);
     }
-
-    public static double AddDouble(double left, double right) => left + right;
-
-    public static double SubtractDouble(double left, double right) => left - right;
-
-    public static double MultiplyDouble(double left, double right) => left * right;
-
-    public static double NegateDouble(double value) => -value;
 
     public static double DivideDouble(double left, double right)
     {
         if (right == 0d)
         {
-            if (left == 0d)
-            {
-                throw new OverflowException("VB6 floating-point division 0 / 0 causes overflow.");
-            }
-
             throw new DivideByZeroException();
         }
 
         return left / right;
     }
 
-    public static double Power(double number, double exponent)
-    {
-        if (number < 0d && exponent != Math.Truncate(exponent))
-        {
-            throw new ArgumentException("VB6 exponentiation requires an integer exponent for a negative base.", nameof(exponent));
-        }
-
-        var result = Math.Pow(number, exponent);
-        if (double.IsInfinity(result))
-        {
-            throw new OverflowException("VB6 exponentiation result is outside the Double range.");
-        }
-
-        if (double.IsNaN(result))
-        {
-            throw new ArithmeticException("VB6 exponentiation produced an invalid numeric result.");
-        }
-
-        return result;
-    }
-
-    public static bool NotBoolean(bool value) => !value;
-
-    public static bool AndBoolean(bool left, bool right) => left & right;
-
-    public static bool OrBoolean(bool left, bool right) => left | right;
-
-    public static bool XorBoolean(bool left, bool right) => left ^ right;
-
-    public static bool EqvBoolean(bool left, bool right) => left == right;
-
-    public static bool ImpBoolean(bool left, bool right) => !left || right;
-
-    public static short NotInteger(short value) => unchecked((short)~value);
-
-    public static int NotLong(int value) => ~value;
-
-    public static long NotLongLong(long value) => ~value;
-
-    public static byte AndByte(byte left, byte right) => (byte)(left & right);
-
-    public static short AndInteger(short left, short right) => (short)(left & right);
-
-    public static int AndLong(int left, int right) => left & right;
-
-    public static long AndLongLong(long left, long right) => left & right;
-
-    public static byte OrByte(byte left, byte right) => (byte)(left | right);
-
-    public static short OrInteger(short left, short right) => (short)(left | right);
-
-    public static int OrLong(int left, int right) => left | right;
-
-    public static long OrLongLong(long left, long right) => left | right;
-
-    public static byte XorByte(byte left, byte right) => (byte)(left ^ right);
-
-    public static short XorInteger(short left, short right) => (short)(left ^ right);
-
-    public static int XorLong(int left, int right) => left ^ right;
-
-    public static long XorLongLong(long left, long right) => left ^ right;
-
-    public static short EqvInteger(short left, short right) => unchecked((short)~(left ^ right));
-
-    public static int EqvLong(int left, int right) => ~(left ^ right);
-
-    public static long EqvLongLong(long left, long right) => ~(left ^ right);
-
-    public static short ImpInteger(short left, short right) =>
-        unchecked((short)((~left & 0xFFFF) | (right & 0xFFFF)));
-
-    public static int ImpLong(int left, int right) => ~left | right;
-
-    public static long ImpLongLong(long left, long right) => ~left | right;
-
-    public static string Concat(object? left, object? right) => VBConversions.CStr(left) + VBConversions.CStr(right);
-
-    public static bool Equal(object? left, object? right) => Compare(left, right) == 0;
-
-    public static bool NotEqual(object? left, object? right) => Compare(left, right) != 0;
-
-    public static bool Less(object? left, object? right) => Compare(left, right) < 0;
-
-    public static bool LessOrEqual(object? left, object? right) => Compare(left, right) <= 0;
-
-    public static bool Greater(object? left, object? right) => Compare(left, right) > 0;
-
-    public static bool GreaterOrEqual(object? left, object? right) => Compare(left, right) >= 0;
-
-    private static float CheckSingle(float value)
-    {
-        if (float.IsInfinity(value))
-        {
-            throw new OverflowException("Value is outside the range of VB6 Single.");
-        }
-
-        return value;
-    }
+    public static string Concat(object? left, object? right) =>
+        VBConversions.CStr(left) + VBConversions.CStr(right);
 
     private static int Compare(object? left, object? right)
     {
-        if (ReferenceEquals(left, right))
+        if (left is string || right is string)
         {
-            return 0;
+            return string.Compare(
+                VBConversions.CStr(left),
+                VBConversions.CStr(right),
+                StringComparison.Ordinal);
         }
 
-        if (left is null)
+        if (left is bool || right is bool)
         {
-            return -1;
+            return VBConversions.CBool(left).CompareTo(VBConversions.CBool(right));
         }
 
-        if (right is null)
+        if (left is VBCurrency || right is VBCurrency)
         {
-            return 1;
+            return VBConversions.CCur(left).CompareTo(VBConversions.CCur(right));
         }
 
-        if (left is string leftString && right is string rightString)
+        return VBConversions.CDbl(left).CompareTo(VBConversions.CDbl(right));
+    }
+
+    private static float CheckFinite(float value)
+    {
+        if (float.IsInfinity(value))
         {
-            return string.CompareOrdinal(leftString, rightString);
+            throw new OverflowException();
         }
 
-        if (left is IComparable comparable)
-        {
-            return comparable.CompareTo(right);
-        }
-
-        throw new InvalidOperationException($"Values of type '{left.GetType()}' cannot be compared.");
+        return value;
     }
 }
 
 public static class VBDebug
 {
-    public static void Print(object? value) => Console.WriteLine(value);
+    public static void Print(object? value)
+    {
+        Console.WriteLine(VBConversions.CStr(value));
+    }
 }
