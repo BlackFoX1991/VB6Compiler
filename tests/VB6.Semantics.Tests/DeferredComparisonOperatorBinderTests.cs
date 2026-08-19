92cf750a6c390f6703c36b77a38cd3baeb9f8a92
Artur Löwen <125445926+BlackFoX1991@users.noreply.github.com>
2026-08-19 12:39:02 +0200

    Test deferred Like and Is semantics

---
+++ tests/VB6.Semantics.Tests/DeferredComparisonOperatorBinderTests.cs
@@ -0,0 +1,53 @@
+using VB6.Parser;
+using VB6.Syntax.Text;
+using ParserType = VB6.Parser.Parser;
+
+namespace VB6.Semantics.Tests;
+
+[TestClass]
+public sealed class DeferredComparisonOperatorBinderTests
+{
+    [TestMethod]
+    public void Bind_LikeProducesDedicatedSemanticDiagnostic()
+    {
+        var model = BindSource("""
+            Sub Main()
+                Dim result As Boolean
+                Dim value As String
+                result = value Like "A*"
+            End Sub
+            """);
+
+        CollectionAssert.AreEqual(
+            new[] { "VB6S0023" },
+            model.Diagnostics.Select(diagnostic => diagnostic.Code).ToArray());
+    }
+
+    [TestMethod]
+    public void Bind_IsProducesDedicatedSemanticDiagnostic()
+    {
+        var model = BindSource("""
+            Sub Main()
+                Dim result As Boolean
+                Dim left As String
+                Dim right As String
+                result = left Is right
+            End Sub
+            """);
+
+        CollectionAssert.AreEqual(
+            new[] { "VB6S0024" },
+            model.Diagnostics.Select(diagnostic => diagnostic.Code).ToArray());
+    }
+
+    private static SemanticModel BindSource(string source)
+    {
+        var text = SourceText.From(source, "test.bas");
+        var parseResult = new ParserType(text).ParseCompilationUnit();
+        Assert.AreEqual(
+            0,
+            parseResult.Diagnostics.Length,
+            string.Join(Environment.NewLine, parseResult.Diagnostics.Select(diagnostic => diagnostic.ToString())));
+        return new Binder(text).BindCompilationUnit(parseResult.Root);
+    }
+}
