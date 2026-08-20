f11904bdabcc0a9172255024b1336d8232d9d091
Artur Löwen <125445926+BlackFoX1991@users.noreply.github.com>
2026-08-20 03:22:42 +0200

    Cover VB6 file I/O parser syntax

---
+++ tests/VB6.Parser.Tests/FileIoParserTests.cs
@@ -0,0 +1,56 @@
+using VB6.Syntax.Nodes;
+using VB6.Syntax.Text;
+using ParserType = VB6.Parser.Parser;
+
+namespace VB6.Parser.Tests;
+
+[TestClass]
+public sealed class FileIoParserTests
+{
+    [TestMethod]
+    public void Parse_PreservesClassicFileIoStatementsWithoutParserErrors()
+    {
+        const string source = """
+            Sub Main()
+                Open "data.bin" For Binary As #1
+                Put #1, 1, value
+                Get #1, 1, value
+                Seek #1, 4
+                Close #1
+                Kill "data.bin"
+            End Sub
+            """;
+
+        var result = new ParserType(SourceText.From(source, "test.bas")).ParseCompilationUnit();
+
+        Assert.AreEqual(
+            0,
+            result.Diagnostics.Length,
+            string.Join(Environment.NewLine, result.Diagnostics.Select(diagnostic => diagnostic.ToString())));
+        var procedure = (SubDeclarationSyntax)result.Root.Members.Single();
+        Assert.AreEqual(6, procedure.Statements.Length);
+        Assert.IsTrue(procedure.Statements.All(statement => statement is FileIoStatementSyntax));
+        CollectionAssert.AreEqual(
+            new[] { "Open", "Put", "Get", "Seek", "Close", "Kill" },
+            procedure.Statements
+                .Cast<FileIoStatementSyntax>()
+                .Select(statement => statement.KeywordToken.Text)
+                .ToArray());
+    }
+
+    [TestMethod]
+    public void Parse_PreservesPrintHashStatement()
+    {
+        const string source = """
+            Sub Main()
+                Print #1, "hello"
+            End Sub
+            """;
+
+        var result = new ParserType(SourceText.From(source, "test.bas")).ParseCompilationUnit();
+
+        Assert.AreEqual(0, result.Diagnostics.Length);
+        var procedure = (SubDeclarationSyntax)result.Root.Members.Single();
+        Assert.IsInstanceOfType<FileIoStatementSyntax>(procedure.Statements.Single());
+    }
+}
