55e2f3f0a3e1414026e8635fd96dcabff7845d6d
Artur Löwen <125445926+BlackFoX1991@users.noreply.github.com>
2026-08-21 10:24:54 +0200

    Normalize metadata hash algorithm naming

---
+++ src/VB6.Emit.Managed/MetadataConstants.cs
@@ -0,0 +1,7 @@
+namespace VB6.Emit.Managed;
+
+internal static class AssemblyHashAlgorithm
+{
+    public const System.Configuration.Assemblies.AssemblyHashAlgorithm Sha256 =
+        System.Configuration.Assemblies.AssemblyHashAlgorithm.SHA256;
+}
