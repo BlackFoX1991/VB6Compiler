4733b0a35b9f0833398bdaddded6a1dd348114ac
Artur Löwen <125445926+BlackFoX1991@users.noreply.github.com>
2026-09-02 19:32:59 +0200

    Test the profile-aware sequential text channels

--- tests/VB6.Compiler.Tests/FileStringIoExecutionTests.cs
+++ tests/VB6.Compiler.Tests/FileStringIoExecutionTests.cs
@@ -1,3 +1,7 @@
+using System.Globalization;
+using System.Text;
+using VB6.Runtime;
+
 namespace VB6.Compiler.Tests;
 
 [TestClass]
@@ -26,5 +30,52 @@ public sealed class FileStringIoExecutionTests
             """);
 
         CollectionAssert.AreEqual(new[] { "Grüße" }, VB6TestProgram.SplitLines(output), output);
+    }
+
+    [TestMethod]
+    public void EmitManagedApplication_PassesTheSelectedProfileToSequentialTextTransfers()
+    {
+        var compilation = VBCompilation.Create(
+            """
+            Sub Main()
+                Dim firstByte As Byte
+                Dim printed As String
+                Dim written As String
+
+                Open "profile-text.txt" For Output As #1
+                Print #1, "ä"
+                Write #1, "ö"
+                Close #1
+
+                Open "profile-text.txt" For Binary As #1
+                Get #1, 1, firstByte
+                Close #1
+
+                Open "profile-text.txt" For Input As #1
+                Line Input #1, printed
+                Input #1, written
+                Close #1
+
+                Debug.Print firstByte
+                Debug.Print printed
+                Debug.Print written
+            End Sub
+            """,
+            "Module1.bas",
+            new VBCompilationOptions
+            {
+                CompatibilityProfile = VBCompatibilityProfile.VB6Sp6
+            });
+
+        Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+        var ansi = Encoding.GetEncoding(
+            0,
+            EncoderFallback.ExceptionFallback,
+            DecoderFallback.ExceptionFallback);
+        var expectedFirstByte = ansi.GetBytes("ä")[0].ToString(CultureInfo.InvariantCulture);
+
+        CollectionAssert.AreEqual(
+            new[] { expectedFirstByte, "ä", "ö" },
+            VB6TestProgram.SplitLines(VB6TestProgram.Run(compilation)));
     }
 }
